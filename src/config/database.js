d1e1256800a8aa9109acd68f04e0bb1ce7128b9c
Alex Vitorino <halexsrv@gmail.com>
2020-02-05 18:51:45 -0300

    initial commit

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
