3cbea08c2926736bb570ac84013cffa0618ab4dd
Alex Vitorino <halexsrv@gmail.com>
2020-02-06 20:27:40 -0300

    add  setup for mongoose

--- src/database/index.js
+++ src/database/index.js
@@ -1,4 +1,5 @@
 import Sequelize from 'sequelize';
+import mongoose from 'mongoose';
 
 import User from '../app/models/User';
 import File from '../app/models/File';
@@ -6,11 +7,14 @@ import Appointment from '../app/models/Appointment';
 
 import databaseConfig from '../config/database';
 
+require('dotenv').config();
+
 const models = [User, File, Appointment];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
 
   init() {
@@ -19,6 +23,13 @@ class Database {
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+    });
   }
 }
 
